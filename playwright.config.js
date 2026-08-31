d2ced4ddd9a0de8bc079786bb6aedff67ea6a936
unknown <abhilash.9prabhakaran@gmail.com>
2026-09-01 04:13:10 +0530

    Configure Playwright headless mode for CI

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    headless: false
+    headless: !!process.env.CI
   },
 
   /* Configure projects for major browsers */
